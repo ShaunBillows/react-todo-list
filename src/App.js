23cfadcc05674aacdd4ee0fbed6909af422446df
ShaunBillows <billows.shaun@gmail.com>
2022-07-09 13:36:25 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react'
+import TodoList from './componants/TodoList'
 
 function App() {
+
+  const [ todos, setTodos ] = useState([])
+  const todoNameRef = useRef()
+
+  const handleAddTodo = (e) => {
+    const name = todoNameRef.current.value
+    if (name ==='') return 
+    console.log(name)
+    setTodos( prevTodos => {
+      return [...prevTodos, {id:Math.random(100), name:name, complete:false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos]
+    const todo = newTodos.find( todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  const getTodosLeft = () => {
+    return todos.filter( todo => !todo.complete ).length
+  }
+
+  const handleClearTodos = () => {
+    const newTodos = todos.filter( todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <>
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+        <input ref={todoNameRef} type="text" />
+        <button onClick={handleAddTodo}>add todo</button> 
+        <button onClick={handleClearTodos}>clear completed todos</button>
+        <div> {getTodosLeft()} left to do</div>
+    </>
+    
+  )
 }
 
 export default App;
---
+++ src/componants/TodoList.js
@@ -0,0 +1,12 @@
+import Todo from './Todo'
+
+const TodoList = ({ todos, toggleTodo }) => {
+
+    return (
+        todos.map( (todo, index) => {
+            return <Todo key={index} todo={todo} toggleTodo={toggleTodo}/>
+        })
+    )
+}
+
+export default TodoList
